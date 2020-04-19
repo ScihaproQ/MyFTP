22fcfe19e35f60a82714f5a5e837da3248b9e578
Manu <emmanuel.lena@epitech.eu>
2020-04-19 20:56:31 +0200

    preliminaries

--- include/ftp.h
+++ include/ftp.h
@@ -8,4 +8,39 @@
 #ifndef NWP_MYFTP_2019_FTP_H
 #define NWP_MYFTP_2019_FTP_H
 
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <errno.h>
+
+int init_server(int port);
+
+int handle_client(int server_fd);
+
+void connected(int fd);
+
+void user(int fd);
+
+void pass(int fd);
+
+void quit(int fd);
+
+typedef void (*messages[])(int);
+
+enum MESSAGES_CODES {
+    CONNECTED,
+    USER,
+    PASS,
+    QUIT,
+};
 #endif //NWP_MYFTP_2019_FTP_H
--- src/main.c
+++ src/main.c
@@ -9,5 +9,16 @@
 
 int main(int ac, char **av)
 {
+    long port = 0;
+    char *end = NULL;
+    int server = 0;
 
+    if (ac != 3)
+        return 84;
+    port = strtol(av[1], &end, 10);
+    if (errno != ERANGE)
+        server = init_server((int)port);
+    if (listen(server, 3) < 0)
+        return 84;
+    handle_client(server);
 }
